c57904475a9165ca8dc3b9a60f9dccb98476f931
Emelie Loheden <emelie_loheden@hotmail.com>
2020-03-09 00:26:01 +0100

    adding albumCover,artistName and  flexbox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Album } from './Components/Album';
 import data from './data.json'
+import 'index.css';
 
 export const App = () => {
-  console.log(data)
+ 
   return (
-    <div class="album-title">
-     {data.albums.items.map(item => {
-        return <Album 
-        key={item.id} 
-        albumTitle={item.name}
-         />
-      })}
-
+    <div><h1>New albums & singles</h1>
+      <div class="album-container">
+        {data.albums.items.map(item => {
+          return <Album
+            key={item.id}
+            albumCover={item.images[1].url}
+            albumTitle={item.name}
+            artistName={item.artists[0].name}
+          />
+        })}
+      </div>
     </div>
 
   )
--- src/Components/Album.js
+++ src/Components/Album.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './album.css';
+import 'index.css';
 
 export const Album = props => {
   return (
-    <div>
-      {props.albumTitle}
-      
-    </div>
+    
+      <div className="album-card">
+       <img src={props.albumCover} alt='Album Cover'></img>
+  <h2 className="album-title">{props.albumTitle}</h2>
+  <h3 className="artist-name">{props.artistName}</h3>
+     </div> 
   )
 }
