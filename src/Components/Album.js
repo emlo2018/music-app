f86c567de990ee730839d1982ffb2b979ddafbe9
Emelie Loheden <emelie_loheden@hotmail.com>
2020-03-13 10:31:47 +0100

    fix icon on hover in css

--- src/Components/Album.js
+++ src/Components/Album.js
@@ -5,16 +5,16 @@ export const Album = props => {
   return (
     
   <section className="album-card">
-   <section className="cover-image">
-    <img src={props.albumCover} alt='Album Cover'></img>
+   <section className="cover">
+    <img className="cover-image" src={props.albumCover} alt='Album Cover'></img>
      <section className="icons">
       <img className="heart" src="icons/heart.svg" alt="heart"></img>
       <img className="play" src="icons/play.svg" alt="play icon"></img>
       <img className="dots" src="icons/dots.svg" alt="dots"></img>
      </section>
    </section>
-  <h2 className="album-title"><a href={props.link}>{props.albumTitle}</a></h2>
-  <h4 className="artist-name"><a href={props.artistLink}>{props.artistName}</a></h4>
+  <a className="album-title" href={props.link}><h2>{props.albumTitle}</h2></a>
+  <a href={props.artistLink}><h4 className="artist-title">{props.artistName}</h4></a>
   </section> 
   )
 }
